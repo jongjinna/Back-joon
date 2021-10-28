d0b6d92d1e9c521b529edc86b93aea13899598ef
jongjin_na <njjwa1004@naver.com>
2021-10-28 21:40:08 +0900

    :memo: 2021.10.28.

---
+++ Python/1000/1920.py
@@ -0,0 +1,11 @@
+import sys
+N = int(input())
+A = list(map(int, sys.stdin.readline().split()))
+M = int(input())
+B = list(map(int, sys.stdin.readline().split()))
+
+for i in B:
+  if i in A:
+    print(1)
+  else:
+    print(0)
