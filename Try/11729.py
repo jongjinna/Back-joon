3140282959d91765f984ef3e24602cf1fa0c1bdd
jongjinna <njjwa1004@naver.com>
2022-02-09 00:33:40 +0900

    2022.02.09

--- Try/11729.py
+++ Try/11729.py
@@ -1,5 +1,19 @@
-a = int(input())
-t = 0
-for i in range(a):
-  t += 2**i
-print(t)
+from ssl import ALERT_DESCRIPTION_RECORD_OVERFLOW
+
+
+def move(N, start, end):
+  print(start, end)
+
+def hanoi(N, start, end, via):
+  if N == 1:
+    move(N, 1, 3)
+  else:
+    hanoi(N-1, start, via, end)
+    move(N, start, end)
+    hanoi(N-1, via, end, start)
+
+
+N = int(input())
+
+print(2**N - 1)
+hanoi(N, 1, 3, 2)
