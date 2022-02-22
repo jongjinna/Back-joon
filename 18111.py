6724fee8038e68def8915d07465da6ced71f1dd8
jongjin_na <80146138+jongjinna@users.noreply.github.com>
2022-02-22 12:50:29 +0900

    Create 18111.py

---
+++ 18111.py
@@ -0,0 +1,28 @@
+N, M, B = map(int, input().split(" "))
+lst = []
+n = 0
+for i in range(N):
+  띄어쓰기로 나누고
+  포문 엔에 총 합 만들기
+
+평균 라운드해서 구하고
+에이는 영
+
+이프 민엔엠이 스몰엔보다 작거나 같으면
+  에베베
+엘잎 민엔엠 - 스몰엠 이 비보다 크면
+  민은 민빼기 1
+  에베베
+엘스
+  에베베
+
+
+에베베 알려쥴게
+for i in lst
+for j in i
+if means -j >0
+a += mean-j
+else
+a -= ....*2
+
+print(a, means)
