123bedfb46d2523d89189482b7197d1139b2385c
jongjin_na <80146138+jongjinna@users.noreply.github.com>
2022-02-22 10:03:19 +0900

    Create 1541.py

---
+++ 1541.py
@@ -0,0 +1,17 @@
+lst = []
+minus = list(input().split("-"))
+for i in minus:
+    lst.append(i.split("+"))
+
+n = 0
+for lst0
+n += int i *2
+
+for i lst
+a=0
+for j in i
+a+=int j
+n -= a
+
+
+print(n)
